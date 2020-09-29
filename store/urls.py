a8c9ab49f1e699470f1894048cbe364f82c40272
alexyak1 <yakimchuk.nstu@gmail.com>
2020-09-29 23:09:31 +0200

    revrite to class oriented view

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, re_path
 
 from . import views
+# from .views import ElectronicsView
+from .views import ElectronicsView3
+
 urlpatterns = [
     path('', views.index, name='index'),
     re_path(r'^\d+', views.detail, name='detail'),
-    re_path(r'^electronics', views.electronics, name='electronics')
+    # re_path(r'^electronics', views.electronics, name='electronics')
+    # re_path(r'^electronics', ElectronicsView.as_view(), name='electronics'),
+    re_path(r'^electronics', ElectronicsView3.as_view(), name='electronics'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.cache import cache_page
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, PageNotAnInteger
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import TemplateView, ListView
+
 
 def index(request):
     return HttpResponse("Hello there, globomantics e-comerce store front coming here...")
@@ -10,18 +13,29 @@ def index(request):
 def detail(request):
     return HttpResponse("Hello there")
 
-@require_http_methods(["GET"])
-# @cache_page(900)
-def electronics(request):
-    items = ("Windows PC", "Apple Mac", "Apple iPhone", "Lenovo", "Samsung", "Google")
-    if request.method == 'GET':
+# @require_http_methods(["GET"])
+# # @cache_page(900)
+class ElectronicsView(View):
+    def get(self, request):
+        items = ("Windows PC", "Apple Mac", "Apple iPhone", "Lenovo", "Samsung", "Google")
         paginator = Paginator(items, 2)
         pages = request.GET.get('page', 1)
+        self.process()
         try:
             items = paginator.page(pages)
         except PageNotAnInteger:
             items = paginator.page(1)
         return render(request, 'store/list.html', {'items': items})
-    elif request.method == 'POST':
-        return HttpResponseNotFound("POST Not Found")
-    
+
+class ElectronicsView2(TemplateView):
+    template_name = 'store/list.html'
+    def get_context_data(self, **kwargs):
+        items = ("Windows PC", "Apple Mac", "Apple iPhone", "Lenovo", "Samsung", "Google")
+        context = {'items': items}
+        return context
+
+class ElectronicsView3(ListView):
+    template_name = 'store/list.html'
+    queryset = ("Windows PC", "Apple Mac", "Apple iPhone", "Lenovo", "Samsung", "Google")
+    context_object_name = 'items'
+    paginate_by = 2

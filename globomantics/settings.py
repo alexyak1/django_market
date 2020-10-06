0076d081eee09d84608bc3ea34800b48ab64a539
alexyak1 <yakimchuk.nstu@gmail.com>
2020-10-06 22:37:24 +0200

    added toolbar

--- globomantics/settings.py
+++ globomantics/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'store'
+    'debug_toolbar',
+    'store',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,25 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = ['127.0.0.1',]
+
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'debug_toolbar.panels.profiling.ProfilingPanel',
 ]
 
 ROOT_URLCONF = 'globomantics.urls'
@@ -127,4 +147,7 @@ CACHES = {
     }
 }
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'root')
+
+STATIC_DIRS = [os.path.join(BASE_DIR, 'static'),]
 # SESSION_ENGINE = "Django.contrib.sessions.backkends.cache"
--- store/urls.py
+++ store/urls.py
@@ -2,8 +2,11 @@ from django.urls import path, re_path
 
 from . import views
 from .views import ElectronicsView
+from django.conf.urls import include
+import debug_toolbar
 
 urlpatterns = [
+    path('__debug__/', include(debug_toolbar.urls)),
     path('', views.index, name='index'),
     re_path(r'^\d+', views.detail, name='detail'),
     # re_path(r'^electronics', views.electronics, name='electronics')
